ff5c53e2c5bfe181bdd819790ecce3b6eb869be6
NIROLK <niromd222@gmail.com>
2024-09-05 14:19:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c8858afdce80eeb01b47e.jpg",
 ALIVE_MSG: process.env.ALIVE_MGG || "👋 HELLO, I AM  DARK NIRO MD I AM ALIVE NOW!",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
