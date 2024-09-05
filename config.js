674ce8e1f1587f8bc89683a3e24ea111aeb56c6a
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-05 22:37:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "3UtFxRjL#EDg7yuV0wZqwyieG8DrsfqguKtPKtN__aXMY3CPqyR8",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c8858afdce80eeb01b47e.jpg"
-ALIVE_MSG: process.env.ALIVE_MSG || "HEY ${pushname}👋\nn👋 HELLO, I AM  DARK NIRO MD I AM ALIVE NOW !"
+ALIVE_MSG: process.env.ALIVE_MSG || "👋 HELLO, I AM  DARK NIRO MD I AM ALIVE NOW !"
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
