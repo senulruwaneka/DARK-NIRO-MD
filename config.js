9ff6e0902e9f55cd4f05d6101b9275d76d08d451
NIROLK <niromd222@gmail.com>
2024-09-05 11:33:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session_id",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c8858afdce80eeb01b47e.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "👋 HELLO, I AM  DARK NIRO MD I AM ALIVE NOW!",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE:"true"
+SESSION_ID: process.env.SESSION_ID,
 };
