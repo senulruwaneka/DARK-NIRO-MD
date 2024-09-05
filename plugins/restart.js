9cf8dad3c934b9d63f24a1b3411378ab055e2797
NIROLK <niromd222@gmail.com>
2024-09-05 09:16:06 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -10,7 +10,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!isOwner) ruturn
+if(!isOwner) return
 const {exec} = require("child_process")
 reply("dark niro md restarting...")
 await sleep(1500)
